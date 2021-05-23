faccc69580ac216ddfdf9ec5be88a86a4d4b1174
stla <laurent_step@yahoo.fr>
2021-05-23 07:47:45 +0200

    import React

---
+++ R/import.R
@@ -0,0 +1,10 @@
+#' Objects imported from other packages
+#' @description These objects are imported from other packages.
+#'   Follow the links to their documentation:
+#'   \code{\link[reactR::React]{React}}.
+#' @importFrom reactR React
+#' @export React
+#' @name shinyChakraUI-imports
+#' @aliases React
+#' @docType import
+NULL

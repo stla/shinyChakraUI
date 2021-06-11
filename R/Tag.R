7f53b29f4c0058b70f8ef7d0af9fd0f0ab98bc7e
stla <laurent_step@yahoo.fr>
2021-06-12 01:41:28 +0200

    Tag

---
+++ R/Tag.R
@@ -0,0 +1,23 @@
+isCapitalized <- function(word){
+  grepl("^[[:upper:]]+$", substring(word, 1, 1))
+}
+
+#' @importFrom htmltools tag
+#' @importFrom rlang dots_list
+#' @export
+#' @noRd
+`$.ReactTagBuilder` <- function(x, name){
+  if(!isCapitalized(name)){
+    stop("Tag name must start with an upper case character", call. = FALSE)
+  }
+  function(...){
+    component <- tag(name, dots_list(...))
+    structure(component, class = c("ReactTag", oldClass(component)))
+  }
+}
+
+#' Title
+#' @description xx
+#' @export
+Tag <- structure(list(), class = "ReactTagBuilder")
+

dfef5c26d9978a6e83d6caf33a7ae7405d40d344
stla <laurent_step@yahoo.fr>
2021-06-25 18:15:44 +0200

    chakraJSX

---
+++ inst/essais/chakraJSX.R
@@ -0,0 +1,53 @@
+library(shiny)
+library(shinyChakraUI)
+
+ui <- chakraPage(
+
+  chakraComponent(
+
+    inputId = "id",
+
+    jsx(paste(
+      '<>',
+      '  <Button onClick={onOpen}>Open Modal</Button>',
+      '  <Modal isOpen={isOpen} onClose={onClose}>',
+      '    <ModalOverlay />',
+      '    <ModalContent>',
+      '      <ModalHeader>Modal Title</ModalHeader>',
+      '      <ModalCloseButton />',
+      '      <ModalBody>',
+      '        Sit nulla est ex deserunt exercitation anim occaecat.',
+      '      </ModalBody>',
+      '      <ModalFooter>',
+      '        <Button colorScheme="blue" mr={3} onClick={onClose}>',
+      '          Close',
+      '        </Button>',
+      '        <Button variant="ghost" onClick={setShinyValue}>',
+      '          Secondary Action',
+      '        </Button>',
+      '      </ModalFooter>',
+      '    </ModalContent>',
+      '  </Modal>',
+      '</>',
+      sep = "\n"
+    ),
+
+    preamble = paste(
+      'const { isOpen, onOpen, onClose } = useDisclosure();',
+      'const setShinyValue = () => Shiny.setInputValue("modal", "action");',
+      sep = "\n"
+    )
+
+  ))
+
+)
+
+server <- function(input, output, session){
+
+  observe({
+    print(input[["modal"]])
+  })
+
+}
+
+shinyApp(ui, server)

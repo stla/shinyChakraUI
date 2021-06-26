636e4026d147ea1608963c15cceb3ecb3f79237a
stla <laurent_step@yahoo.fr>
2021-06-26 18:43:19 +0200

    chakraCheckboxWithChildren

---
+++ R/chakraCheckboxWithChildren.R
@@ -0,0 +1,47 @@
+#' @title Checkbox with child checkboxes
+#' @description A widget with a parent checkbox and child checkboxes.
+#'
+#' @param inputId widget id
+#' @param parentCheckbox the parent checkbox
+#' @param ... the child checkboxes
+#' @param stackAttributes list of attributes which control the layout
+#'
+#' @return A widget to use in \code{\link{chakraComponent}}.
+#' @export
+#' @importFrom htmltools tags
+#'
+#' @examples
+chakraCheckboxWithChildren <- function(
+  inputId, parentCheckbox, ...,
+  stackAttributes = list(pl = 6, mt = 1, spacing = 1)
+){
+  stopifnot(isChakraCheckbox(parentCheckbox))
+  if(!isNamedList(stackAttributes)){
+    stop(
+      "Invalid `stackAttributes` argument; it must be a named list.",
+      call. = TRUE
+    )
+  }
+  checkboxes <- list(...)
+  if(
+    length(checkboxes) == 0L ||
+    !all(vapply(checkboxes, isChakraCheckbox, logical(1L)))
+  ){
+    stop(
+      "You must provide some checkboxes in the `...` argument.",
+      call. = TRUE
+    )
+  }
+  tags$div(
+    id = inputId,
+    class = "checkboxWithChildren",
+    parentCheckbox,
+    asShinyTag(
+      list(
+        name = "Stack",
+        attribs = stackAttributes,
+        children = checkboxes
+      )
+    )
+  )
+}

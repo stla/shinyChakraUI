d10914138d80ece3bda5f8e1688e4376f8574c84
stla <laurent_step@yahoo.fr>
2021-06-26 13:06:03 +0200

    doc useClipboard

--- R/jseval_and_hooks.R
+++ R/jseval_and_hooks.R
@@ -114,15 +114,70 @@ setState <- function(state, value){
   }
 }
 
-#' Title
+#' @title The `useClipboard` hook
+#' @description  The `useClipboard` hook.
 #'
-#' @param value
+#' @param value a string
 #'
-#' @return
 #' @export
 #' @importFrom utils URLencode
 #'
 #' @examples
+#' library(shiny)
+#' library(shinyChakraUI)
+#'
+#' ui <- chakraPage(
+#'
+#'   br(),
+#'
+#'   chakraComponent(
+#'     "mycomponent",
+#'
+#'     withStates(
+#'       Tag$Box(
+#'         width = "50%",
+#'
+#'         Tag$Flex(
+#'           mb = 2,
+#'           Tag$Input(
+#'             isReadOnly = TRUE,
+#'             value = getHookProperty("clipboard", "value")
+#'           ),
+#'           Tag$Button(
+#'             ml = 2,
+#'             onClick = getHookProperty("clipboard", "onCopy"),
+#'             jseval('getState("hasCopied") ? "Copied" : "Copy"')
+#'           )
+#'         ),
+#'
+#'         br(),
+#'         Tag$Divider(),
+#'         br(),
+#'
+#'         Tag$Editable(
+#'           bg = "yellow.100",
+#'           placeholder = "Paste here",
+#'           Tag$EditablePreview(),
+#'           Tag$EditableInput()
+#'         )
+#'
+#'       ),
+#'
+#'       states = list(
+#'         clipboard = useClipboard("Hello Chakra"),
+#'         hasCopied = getHookProperty("clipboard", "hasCopied")
+#'       )
+#'     )
+#'
+#'   )
+#'
+#' )
+#'
+#' server <- function(input, output, session){}
+#'
+#' if(interactive()){
+#'   shinyApp(ui, server)
+#' }
 useClipboard <- function(value){
   stopifnot(isString(value) || isJseval(value))
   if(is.character(value)){

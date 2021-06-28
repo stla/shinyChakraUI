776e04dfadeaaa525cc7aa50662baa7ca7f6b7c9
stla <laurent_step@yahoo.fr>
2021-06-28 13:08:32 +0200

    doc setReactState

--- R/chakra.R
+++ R/chakra.R
@@ -1,16 +1,81 @@
-
-#' Title
+#' @title Set a React state
+#' @description Set a React state from the Shiny server.
 #'
-#' @param session
-#' @param componentId
-#' @param stateName
-#' @param value
+#' @param session Shiny session object
+#' @param componentId the id of the \code{\link{chakraComponent}} which
+#'   contains the state to be changed
+#' @param stateName the name of the state to be set
+#' @param value the new value of the state
 #'
-#' @return
 #' @export
 #' @importFrom utils URLencode
 #'
 #' @examples
+#' library(shiny)
+#' library(shinyChakraUI)
+#'
+#' ui <- chakraPage(
+#'
+#'   br(),
+#'
+#'   chakraComponent(
+#'     "mycomponent",
+#'
+#'     Tag$Button(
+#'       id = "button",
+#'       className = "action-button",
+#'       colorScheme = "facebook",
+#'       display = "block",
+#'       onClick = jseval("(event) => {event.target.disabled = true}"),
+#'       "Click me to change the content of the container"
+#'     ),
+#'
+#'     br(),
+#'     Tag$Divider(),
+#'     br(),
+#'
+#'     withStates(
+#'
+#'       Tag$Container(
+#'         maxW = "xl",
+#'         centerContent = TRUE,
+#'         bg = "yellow.100",
+#'         getState("containerContent")
+#'       ),
+#'
+#'       states = list(containerContent = "I am the container content.")
+#'
+#'     )
+#'
+#'   )
+#'
+#' )
+#'
+#' server <- function(input, output, session){
+#'
+#'   observeEvent(input[["button"]], {
+#'
+#'     setReactState(
+#'       session = session,
+#'       componentId = "mycomponent",
+#'       stateName = "containerContent",
+#'       value = Tag$Box(
+#'         padding = "4",
+#'         maxW = "3xl",
+#'         fontStyle = "italic",
+#'         fontWeight = "bold",
+#'         borderWidth = "2px",
+#'         "I am the new container content, included in a Box."
+#'       )
+#'     )
+#'
+#'   })
+#'
+#' }
+#'
+#' if(interactive()){
+#'   shinyApp(ui, server)
+#' }
 setReactState <- function(session, componentId, stateName, value){
   if(is.null(statesEnvir[[componentId]])){
     rm(list = ls(statesEnvir), envir = statesEnvir)
@@ -37,7 +102,7 @@ setReactState <- function(session, componentId, stateName, value){
   if(inherits(value, "html")){
     type <- "html"
     value <- URLencode(as.character(value))
-  }else if(inherits(value, "shiny.tag")){
+  }else if(isShinyTag(value)){
     type <- "component"
     value[["attribs"]][["shinyValue"]] <- FALSE
     value <- unclassComponent(value, NULL, "setReactState")[["component"]]
@@ -120,30 +185,6 @@ chakraPage <- function(...){
         package = "shinyChakraUI"
       )
     )
-  )
-}
-
-#' <Add Title>
-#'
-#' <Add Description>
-#'
-#' @importFrom reactR createReactShinyInput
-#' @importFrom htmltools htmlDependency tags
-#' @noRd
-chakraInput <- function(inputId, configuration, default = NULL) {
-  createReactShinyInput(
-    inputId = inputId,
-    class = "chakra",
-    dependencies = htmlDependency(
-      name = "chakra-input",
-      version = "1.0.0",
-      src = "www/shinyChakraUI/chakra",
-      package = "shinyChakraUI",
-      script = "chakra.js"
-    ),
-    default = list(value = default, widget = configuration[["widget"]]),
-    configuration = configuration,
-    container = tags$div
   )
 }
 
@@ -521,213 +562,3 @@ chakraMenuDivider <- function(){
   divider
 }
 
-#' Title
-#'
-#' @param inputId
-#'
-#' @return
-#' @export
-#'
-#' @examples
-chakraAlertInput <- function(inputId) {
-  component <- list(
-    element = "Alert",
-    props = list(
-      status = "success",
-      variant = "subtle",
-      flexDirection = "column",
-      alignItems = "center",
-      justifyContent = "center",
-      textAlign = "center",
-      height = "200px"
-    ),
-    children = list(
-      list(
-        element = "AlertIcon",
-        props = list(
-          boxSize = "40px",
-          mr = 0
-        )
-      ),
-      list(
-        element = "AlertTitle",
-        props = list(
-          mt = 4,
-          mb = 1,
-          fontSize = "lg"
-        ),
-        children = list(
-          "Application submitted!"
-        )
-      ),
-      list(
-        element = "AlertDescription",
-        props = list(
-          maxWidth = "sm"
-        ),
-        children = list(
-          "Thanks for submitting your application. Our team will get back to you soon."
-        )
-      )
-    )
-  )
-  chakraInput(
-    inputId = inputId,
-    configuration = list(widget = "alert", component = component)
-  )
-}
-
-
-#' Title
-#'
-#' @param inputId
-#' @param menuButton
-#'
-#' @return
-#' @export
-#'
-#' @examples
-chakraMenuInput <- function(inputId, menuButton, menuList, closeOnSelect = TRUE){
-  content <- menuList[["children"]]
-  elements <- vapply(content, `[[`, FUN.VALUE = character(1L), "name")
-  menuoptiongroups <- which(elements == "MenuOptionGroup")
-  if(length(menuoptiongroups)){
-    values <- lapply(content[menuoptiongroups], function(optiongroup){
-      attr(optiongroup, "values")
-    })
-    titles <- lapply(content[menuoptiongroups], function(optiongroup){
-      optiongroup[["attribs"]][["title"]]
-    })
-    names(values) <- titles
-  }
-  component <- list(
-    name = "Fragment",
-    attribs = list(),
-    children = list(
-      menuButton,
-      menuList
-      # list(
-      #   element = "MenuList",
-      #   props = list(),
-      #   children = list(
-      #     list(
-      #       element = "MenuItem",
-      #       props = list(),
-      #       children = list(
-      #         "Download"
-      #       )
-      #     )
-      #   )
-      # )
-    )
-  )
-  chakraInput(
-    inputId = inputId,
-    configuration =
-      list(
-        widget = if(length(menuoptiongroups)) "menuWithGroups" else "menu",
-        component = unclassComponent(component),
-        text = attr(menuButton, "text"),
-        closeOnSelect = closeOnSelect,
-        optiongroups =
-          if(length(menuoptiongroups)) as.list(menuoptiongroups - 1L)
-      ),
-    default = if(length(menuoptiongroups)) values
-  )
-}
-
-
-
-# chakraAlertDialogInput <- function(inputId){
-#   component <- list(
-#     element = "Fragment",
-#     props = list(),
-#     children = list(
-#       list(
-#         element = "OpenButton",
-#         props = list(
-#           colorScheme = "red"
-#         ),
-#         children = list(
-#           "Delete customer"
-#         )
-#       ),
-#       list(
-#         element = "AlertDialog",
-#         props = list(),
-#         children = list(
-#           list(
-#             element = "AlertDialogOverlay",
-#             props = list(),
-#             children = list(
-#               list(
-#                 element = "AlertDialogContent",
-#                 props = list(),
-#                 children = list(
-#                   list(
-#                     element = "AlertDialogHeader",
-#                     props = list(
-#                       fontSize = "lg",
-#                       fontWeight = "bold"
-#                     ),
-#                     children = list(
-#                       "Delete customer"
-#                     )
-#                   ),
-#                   list(
-#                     element = "AlertDialogBody",
-#                     props = list(),
-#                     children = list(
-#                       "Are you sure? You can't undo this action afterwards"
-#                     )
-#                   ),
-#                   list(
-#                     element = "AlertDialogFooter",
-#                     props = list(),
-#                     children = list(
-#                       list(
-#                         element = "CancelButton",
-#                         props = list(
-#                           id = "cancel"
-#                         ),
-#                         children = list(
-#                           "Cancel"
-#                         )
-#                       ),
-#                       list(
-#                         element = "Button",
-#                         props = list(
-#                           id = "delete",
-#                           colorScheme = "red",
-#                           ml = 3
-#                         ),
-#                         children = list(
-#                           "Delete"
-#                         )
-#                       )
-#                     )
-#                   )
-#                 )
-#               )
-#             )
-#           )
-#         )
-#       )
-#     )
-#   )
-#   chakraInput(
-#     inputId = inputId,
-#     configuration = list(widget = "alertdialog", component = component)
-#   )
-# }
-
-#' <Add Title>
-#'
-#' <Add Description>
-#'
-#' @export
-updateChakraInput <- function(session, inputId, value, configuration = NULL) {
-  message <- list(value = value)
-  if (!is.null(configuration)) message$configuration <- configuration
-  session$sendInputMessage(inputId, message);
-}
